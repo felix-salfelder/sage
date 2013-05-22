0e3a0e6b8d8b9ef48a43cd90d30eb9313659ddc1
Felix Salfelder <felix@salfelder.org>
2013-05-22 15:14:52 +0200

    python-sage. automake bug workaround
    there's an automake bug [1] that confuses .pyx->.c and .pyx->.cc rules.
    the workaround is creating all Makefiles one by one from autogen.sh.
    recreated Makefile.am's are fine.
    
    the bug has been fixed upstream, this workaround can be removed
    - once the fixed autotools has been released or
    - after switching to sane file extensions.
    
    [1] http://debbugs.gnu.org/cgi/bugreport.cgi?bug=14441

--- src/sage/autogen.sh
+++ src/sage/autogen.sh
@@ -5,3 +5,9 @@ libtoolize
 aclocal $ACLOCAL_FLAGS
 autoconf
 automake --add-missing
+
+for i in $( find . -name Makefile.am ); do
+	a=${i%.am}; b=${a#./}
+	echo "/bin/sh missing --run automake-1.11 $b"
+	/bin/sh missing --run automake-1.11 $b
+done
